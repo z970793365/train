61f8de10eb41c35043748f13135678b0c23cfd30
z970793365 <970793365@qq.com>
2020-06-21 13:41:41 +0800

    第四次

---
+++ 4/main.js
@@ -0,0 +1,53 @@
+// 弹幕定时器
+var timers = [];
+// 控制弹幕显隐变量
+var isShow = true;
+// 监听发送按钮
+$(".send").on("click", function () {
+    // 创建弹幕
+    var jqueryDom = createScreenbullet($("#screenBulletText").val());
+    // 添加定时任务
+    addInterval(jqueryDom);
+});
+// 监听关闭弹幕按钮
+$(".clear").on("click", function () {
+    if (isShow) {
+        $(".bullet").css("opacity", 0);
+        isShow = false;
+    } else {
+        $(".bullet").css("opacity", 1);
+        isShow = true;
+    }   
+});
+// 新建一个弹幕
+function createScreenbullet(text) {
+    var jqueryDom = $("<div class='bullet'>" + text + "</div>");
+    var fontColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random()) + ")";
+    var fontSize = Math.floor((Math.random() + 1) * 24) + "px";
+    var left = $(".screen_container").width() + "px";
+    var top = Math.floor(Math.random() * 400) + "px";
+    top = parseInt(top) > 352 ? "352px" : top;
+    jqueryDom.css({
+        "position": 'absolute',
+        "color": fontColor,
+        "font-size": fontSize,
+        "left": left,
+        "top": top
+    });
+    $(".screen_container").append(jqueryDom);
+    return jqueryDom;
+}
+// 为弹幕添加定时任务
+function addInterval(jqueryDom) {
+    var left = jqueryDom.offset().left - $(".screen_container").offset().left;
+    var timer = setInterval(function () {
+        left--;
+        jqueryDom.css("left", left + "px");
+        if (jqueryDom.offset().left + jqueryDom.width() < $(".screen_container").offset().left) {
+            jqueryDom.remove();
+            clearInterval(timer);
+        }
+    }, 
+    10);
+    timers.push(timer);
+}
